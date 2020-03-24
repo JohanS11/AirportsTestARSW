839dfd39c13624a5a0fcfaca06dc5a6b53e493c7
JohanS11 <johan.arias@mail.escuelaing.edu.co>
2020-03-24 10:01:58 -0500

    adding fonts

---
+++ src/main/java/com/airportstest/arias/AirportstestArswApplication.java
@@ -0,0 +1,13 @@
+package com.airportstest.arias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportstestArswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportstestArswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airportstest/arias/AirportstestArswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airportstest.arias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportstestArswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
